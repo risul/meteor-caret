7eae4403a00d863cf81ac0518049b3679cdc84a0
root <risul.islam@regfire.com>
2015-04-08 15:46:07 +0200

    Package published

---
+++ README.md
@@ -0,0 +1,10 @@
+meteor-caret.js
+===============
+
+Meteor package for Caret.js - Get caret postion and offset from text field
+More info: https://github.com/ichord/Caret.js
+
+##Install
+```bach
+meteor add risul:caret
+```
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+    name: 'risul:caret',
+    summary: 'Meteor package for Caret.js - Get caret postion and offset from text field',
+    version: '0.0.2',
+    git: 'https://github.com/risul/meteor-caret'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('METEOR@1.0');
+    api.use("jquery", "client");
+    api.addFiles('lib/jquery.caret.js', ['client']);
+});
